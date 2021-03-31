0c8b311e30f49f385212d1daf9d448a1a0623f0c
eugène <eugenevalty@gmail.com>
2021-03-31 17:05:18 +0200

    Commenté le code des diffuseurs et ajouté des variables statiques

--- Diffuseur.java
+++ Diffuseur.java
@@ -6,14 +6,15 @@ public class Diffuseur {
     private String[] msgList;
     private final int port1, port2;
     private final String ipmulti, ip2;
-    private int num_msg = 0;
+    private int NUM_MSG = 0;
+    private static int SLEEP_TIME = 5000;
 
-    Diffuseur(String id, int port1, int port2, String ipmulti) throws IOException {
-        if (id.length() < 8) {
-            for (int i = id.length(); i < 8; i++) {
+    // Diffuseur identifié par :
+    // un id, un port et une adresse multi diff, et un port pour la communication connectée
+    public Diffuseur(String id, int port1, int port2, String ipmulti) throws IOException {
+        if (id.length() < 8) 
+            for (int i = id.length(); i < 8; i++)
                 id += "#";
-            }
-        }
         this.id = id;
         this.port1 = port1;
         this.port2 = port2;
@@ -22,11 +23,17 @@ public class Diffuseur {
         msgList = new String[10000];
     }
 
-    void incrMsg() {
-        num_msg = (num_msg + 1) % 10000;
+    // Permet d'incrémenter le nombre de message
+    // TODO...
+    // Gérer la concurrence multi thread
+    private void incrMsg() {
+        NUM_MSG = (NUM_MSG + 1) % 10000;
     }
 
-    void addMsg(byte[] data, String s) {
+    // Met s dans le tableau data
+    // - Remplit de \0 si trop petit
+    // - Tronque si trop grand
+    private void stringInBytes(byte[] data, String s) {
         byte[] newS = s.getBytes();
         for (int i = 0; i < data.length; i++) {
             if (i < newS.length)
@@ -36,7 +43,8 @@ public class Diffuseur {
         }
     }
 
-    String normalizedNumMsg(int num_msg) {
+    // Normalise un numéro de message
+    private String normalizedNumMsg(int num_msg) {
         if (num_msg < 10)
             return "000" + num_msg;
         if (num_msg < 100)
@@ -46,40 +54,47 @@ public class Diffuseur {
         return String.valueOf(num_msg);
     }
 
-    void diffuse() {
+    // Fonction utilisée pour la diffusion
+    private void diffuse() {
         try (DatagramSocket dso = new DatagramSocket()) {
             System.out.println("JE SUIS LE DIFFUSEUR " + id);
             byte[] data = new byte[4 + 1 + 4 + 1 + 8 + 1 + 140];
             int i = 0;
             while (true) {
+                // On envoie un message en multi-diffusion
                 InetSocketAddress ia = new InetSocketAddress(ipmulti, port1);
                 String s = "Je suis un message pret a etre envoye et j'aime la confiture sa mere " + i++;
                 msgList[num_msg] = s;
-                addMsg(data, "DIFF " + normalizedNumMsg(this.num_msg) + " " + id + " " + s);
+                stringInBytes(data, "DIFF " + normalizedNumMsg(this.num_msg) + " " + id + " " + s);
                 System.out.println("JE DIFFUSE UN MESSAGE À L'ADRESSE " + ipmulti + " ET SUR LE PORT " + port1);
                 DatagramPacket paquet = new DatagramPacket(data, data.length, ia);
+                // On envoie le paquet
                 dso.send(paquet);
+                // On incrémente le nombre de message
                 incrMsg();
                 System.out.println("PAQUET ENVOYE " + normalizedNumMsg(this.num_msg));
-                Thread.sleep(5000);
+                // Puis on dort avant d'envoyer un nouveau message
+                Thread.sleep(SLEEP_TIME);
             }
-        } catch (Exception se) {
-            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    void start() {
-        new Thread(() -> {
-            diffuse();
-        }).start();
+    public void start() {
+        // On créer un thread qui va diffuser 
+        new Thread(() -> diffuse()).start();
+        // Puis on attend des connexions TCP
         try (ServerSocket sso = new ServerSocket(port2)) {
             while (true) {
                 Socket sock = sso.accept();
                 System.out.println("NOUVELLE CO");
+                // On créer un nouveau thread pour la connexion entrante
                 new Thread(() -> {
                     try (BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()))) {
                         char[] command = new char[4];
                         br.read(command, 0, 4);
+                        // On regarde quelle commande est effectuée
                         switch (String.valueOf(command)) {
                         case "LAST":
                             lastMsgs(sock, br);
@@ -88,9 +103,9 @@ public class Diffuseur {
                             addToSend(sock, br);
                             break;
                         default:
+                            System.out.println("COMMANDE INCONNUE");
                             sock.close();
                             break;
-
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -104,22 +119,27 @@ public class Diffuseur {
 
     void lastMsgs(Socket sock, BufferedReader br) throws IOException {
         System.out.println("LAST MSG");
+        // On saute l'espace
         br.read();
         char[] nb = new char[3];
+        // On récupère le nombre de message demandé
         if (br.read(nb, 0, 3) < 3) {
             br.close();
             return;
         }
-        System.out.println("NBRES");
+        // Pour ne pas dépasser la taille du nombre de message diffusé
         int nbres = Math.min(Integer.valueOf(String.valueOf(nb)), num_msg - 1);
         PrintWriter pw = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+        // Puis on envoie les `nbres` derniers messages
         for (int i = 0; i < nbres; i++) {
             String s = "OLDM " + normalizedNumMsg(this.num_msg - i) + " " + id + " " + msgList[num_msg - i - 1];
             byte[] data = new byte[4 + 1 + 4 + 1 + 8 + 1 + 140];
-            addMsg(data, s);
-            System.out.println(new String(data, 0, data.length).length());
+            stringInBytes(data, s);
+            // Puis on envoie les données
             pw.print(new String(data));
+            pw.flush();
         }
+        // On finit par envoyer `ENDM` et on ferme la connexion
         pw.print("ENDM");
         pw.flush();
         sock.close();
